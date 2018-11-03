6e3ed5792ed97f6868b44f243f834be9f9166257
linyuta <linyuta1230@gmail.com>
2018-11-03 13:05:16 +0800

    go programming learning path

---
+++ Hello-world.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Everyone! This is go programming")
+
+	foo()
+
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func bar() {
+	fmt.Println("We're excited")
+}
