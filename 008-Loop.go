351ac1a2c60556211b23cb7d4a02eecd05515515
linyuta <linyuta1230@gmail.com>
2018-11-03 16:07:36 +0800

    add nesting loops

---
+++ 008-Loop.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+// There's no while in loop
+
+func main() {
+
+	// for init; condition; post{ }
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	// Nesting loops
+	for i := 0; i <= 10; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Printf("The outer loop: %d\t The inner loop: %d\n", i, j)
+		}
+	}
+
+}
