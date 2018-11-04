91f21b7f903713677e9c667d283c0d0e288baf53
linyuta <linyuta1230@gmail.com>
2018-11-04 16:25:41 +0800

    add multidimension int slice example

--- 014-MultiDimension-Slice.go
+++ 014-MultiDimension-Slice.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+
+	// Example 1
 	records := make([][]string, 0)
 
 	// Student 1
@@ -30,4 +32,18 @@ func main() {
 	// print
 	fmt.Println(records)
 
+	// Example 2
+
+	transactions := make([][]int, 0)
+
+	for i := 0; i < 3; i++ {
+		transaction := make([]int, 0)
+
+		for j := 0; j < 4; j++ {
+			transaction = append(transaction, j)
+		}
+		transactions = append(transactions, transaction)
+	}
+
+	fmt.Println(transactions)
 }
